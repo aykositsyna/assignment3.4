4a5ef154e3c9b6723b8224c2ca261b9c7b9edfe3
Alina <a.y.kositsyna@gmail.com>
2023-11-21 23:19:14 +0700

    (feat)[ModelLayer]: created class StudentArgs

---
+++ assignment3.4/StudentArgs.cs
@@ -0,0 +1,18 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class StudentArgs : EventArgs
+    {
+        public Student Student { get; set; }
+        public StudentArgs(Student student)
+        {
+            Student = student;
+        }
+    }
+}
