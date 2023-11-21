3e73fc50f91ac8ec72bfb5985af78de42e6b46b9
Alina <a.y.kositsyna@gmail.com>
2023-11-21 23:18:12 +0700

    (feat)[ModelLayer]: created class Model

---
+++ assignment3.4/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class Model : IModel
+    {
+        public IList<Student> Students { get; set; } = new List<Student>();
+
+        public event EventHandler<StudentArgs> EventStudentAddModel
+            = delegate { };
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+
+            EventStudentAddModel(this, new StudentArgs(student));
+        }
+
+    }
+}
