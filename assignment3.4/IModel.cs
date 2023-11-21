b87246192a937653ab922949140ca284f4456380
Alina <a.y.kositsyna@gmail.com>
2023-11-21 23:14:23 +0700

    (feat)[ModelLayer]: created interface IModel

---
+++ assignment3.4/IModel.cs
@@ -0,0 +1,16 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public interface IModel
+    {
+        void AddStudent(Student student);
+
+        event EventHandler<StudentArgs> EventStudentAddModel;
+    }
+}
