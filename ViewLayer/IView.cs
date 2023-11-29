a8ffde74dadc3f125e48abe974fee72ab46c7380
Alina <a.y.kositsyna@gmail.com>
2023-11-29 14:33:37 +0700

    (feat)[ViewLayer]: created interface IView and studentEventArgs

---
+++ ViewLayer/IView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewLayer
+{
+    public interface IView
+    {
+        event Action<StudentEventArgs> AddEvent;
+        event Action<int> DeleteEvent;
+
+        void UpdateData(IEnumerable<StudentEventArgs> studentEventArgs);
+    }
+}
---
+++ ViewLayer/StudentEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewLayer
+{
+    public class StudentEventArgs : EventArgs
+    {
+        public string name {  get; set; }
+
+        public string speciality { get; set; }
+
+        public string group { get; set; }
+    }
+}
