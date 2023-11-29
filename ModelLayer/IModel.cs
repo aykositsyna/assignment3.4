c18dc76c9e5cb0ce8cc4f195bd0ce3cfc4d29915
Alina <a.y.kositsyna@gmail.com>
2023-11-29 14:31:02 +0700

    (feat)[ModelLayer]: created interface IModel

---
+++ ModelLayer/IModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public interface IModel
+    {
+        void Add(Student student);
+
+        void Delete(int index);
+
+        event EventHandler<IEnumerable<Student>> DataSourceChanged;
+    }
+}
