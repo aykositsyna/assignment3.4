d011c109ca31d6afe1db6d64b1951309b1547191
Alina <a.y.kositsyna@gmail.com>
2023-11-29 14:32:39 +0700

    (feat)[Presenter]: created Presenter

---
+++ Presenter/Presenter.cs
@@ -0,0 +1,40 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewLayer;
+
+namespace Presenter
+{
+    public class Presenter
+    {
+        IView view;
+        IModel model;
+
+        public Presenter ()
+        {
+            view.AddEvent += View_OnAdd;
+            view.DeleteEvent += View_OnDelete;
+        }
+
+        public void View_OnAdd(StudentEventArgs e)
+        {
+            model.Add(new Student());
+        }
+
+        public void View_OnDelete(int index)
+        {
+            model.Delete(index);
+        }
+
+        public void Model_DataChanged(IEnumerable<StudentEventArgs> studentEventArgs)
+        {
+            view.UpdateData(studentEventArgs);
+        }
+
+
+    }
+}
