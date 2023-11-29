5a68bef30e52c035105086f650413d6cf7f87340
Alina <a.y.kositsyna@gmail.com>
2023-11-29 14:32:00 +0700

    (feat)[ModelLayer]: created clss Model : IModel and class Student

---
+++ ModelLayer/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class Model : IModel
+    {
+        private List<Student> Students = new List<Student>()
+        {
+            new Student() {Name = "Ivan", Speciality = "History", Group = "AB22-02"},
+            new Student() {Name = "Egor", Speciality = "SKD", Group = "AB10-01"}
+        };
+
+        public event EventHandler<IEnumerable<Student>> DataSourceChanged ;
+
+        public void Add(Student student)
+        {
+            Students.Add(student);
+            DataSourceChanged?.Invoke(this, Students);
+        }
+
+        public void Delete(int index)
+        {
+            Students.RemoveAt(index);
+            DataSourceChanged?.Invoke(this, Students);
+        }
+    }
+}
