50fd4cee076d9ed1b8c89b576c376c4638651fe3
Alina <a.y.kositsyna@gmail.com>
2023-12-04 21:58:57 +0700

    (feat)[Presenter Program]: tried ti write Presenter Program

--- Presenter/Program.cs
+++ Presenter/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using ViewLayer;
 
 namespace Presenter
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form form1 = new Form1();
+            form1.Show();
+            Application.Run();
+            Environment.Exit(0);
         }
     }
 }
